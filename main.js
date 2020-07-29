c785aec51d292ce4d27dd791f2d4dc1ec719a322
sowjanya <sb28@illinois.edu>
2020-07-29 10:58:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,6 @@
 
 
 
-xdomain = [0,1,2,3,4,5];
-xrange = [0,200];
-ydomain = [4,42];
-yrange = [180.9524,0];
-hdomain = [0,42];
-hrange = [0,200];
-xs= d3.scaleBand().domain(xdomain).range(xrange)
-ys= d3.scaleLinear().domain(ydomain).range(yrange)
-hs= d3.scaleLinear().domain(hdomain).range(hrange)
-
 var margin =50;
 var data = [4,8,15,16,23,42];
 
@@ -22,7 +12,7 @@ g.selectAll("rect")
 .data(data)
 .enter()
 .append("rect")
-.attr("x", function(d,i){return xs(i);})
-.attr("y", function(d){return ys(d);})
+.attr("x", function(d,i){return i;})
+.attr("y", function(d){return d;})
 .attr("width", 33.3333)
-.attr("height", function(d){return hs(d);})
+.attr("height", function(d){return d;})
