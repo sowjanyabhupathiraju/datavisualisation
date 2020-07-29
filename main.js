aae278f211c054141b191664680a8605bbfe1df2
sowjanya <sb28@illinois.edu>
2020-07-29 08:44:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-async function init() {
+
 const data = await d3.csv('https://flunky.github.io/cars2017.csv');
 var logScalex = d3.scaleLog().base(10)
   .domain([10, 150])
@@ -9,17 +9,6 @@ var logScaley = d3.scaleLog().base(10)
   .range([200, 0]);
 var margin = 50;
 
-
-d3.select('svg').attr("width", 400).attr("height", 400)
-.append("g")
-.attr("transform", "translate(50,50)" )
-.selectAll('circle')
-.data(data)
-.enter()
-.append('circle')
-.attr('cx', function(d,i){return logScalex(d.AverageCityMPG);})
-.attr('cy', function(d,i){return logScaley(d.AverageHighwayMPG);})
-.attr('r', function(d,i){return d.EngineCylinders*2 -(d.EngineCylinders)+2 ;})
 
 var logScaleaxisx = d3.scaleLog().base(10)
   .domain([10, 150])
@@ -35,4 +24,3 @@ d3.select('svg').append("g").attr("transform","translate(50,50)").call((y_axis).
 var x_axis = d3.axisBottom().scale(logScaleaxisx);
 d3.select('svg').append("g").attr("transform","translate(50,250)").call((x_axis).tickValues([10,20,50,100]).tickFormat(d3.format("~s")))
 
-}
